4ae5aed0e50573d9230baf7af1380f00afd5cdf4
YuDashuai <970972807@qq.com>
2021-09-03 03:16:04 +0800

    update for release

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
 #include "MainWindow.h"
 
 #include <QApplication>
-
+#include "windows.h"
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     MainWindow w;
     w.show();
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
     return a.exec();
 }
